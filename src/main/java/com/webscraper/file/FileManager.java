4ed3bb13fdfcb9c2e0dec93390b1e04b2a3d0965
Dawid <prawica.dawid@gmail.com>
2018-07-19 19:59:02 +0200

    file manager

--- src/main/java/com/webscraper/file/FileManager.java
+++ src/main/java/com/webscraper/file/FileManager.java
@@ -1,4 +1,20 @@
 package com.webscraper.file;
 
 public class FileManager {
+
+    public String readFile(String fileName){
+        return null;
+    }
+
+    public void addToFile(String txt){
+
+    }
+
+    public void createFile(String fileName){
+
+    }
+
+    public String[] listFiles(){
+        return null;
+    }
 }
