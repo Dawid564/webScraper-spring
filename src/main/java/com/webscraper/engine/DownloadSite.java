a2127714a6ccecd72bf0b8f93f932bb17452844a
Dawid <prawica.dawid@gmail.com>
2018-07-24 19:33:12 +0200

    link transport flow

--- src/main/java/com/webscraper/engine/DownloadSite.java
+++ src/main/java/com/webscraper/engine/DownloadSite.java
@@ -2,10 +2,14 @@ package com.webscraper.engine;
 
 import java.io.FileOutputStream;
 import java.net.URL;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
 public class DownloadSite {
+
+    private String web = null;
 
     public DownloadSite(String link){
         download(link);
@@ -16,12 +20,29 @@ public class DownloadSite {
         try {
             website = new URL(link);
             ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            ByteBuffer bb = ByteBuffer.allocate(10);
 
-            System.out.println();
-            FileOutputStream fos = new FileOutputStream("info.html");
-            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            System.out.println(rbc.read(ByteBuffer.allocate(10)));
+            int n;
+            String msg = "";
+            while((n=rbc.read(bb)) > -1){
+                bb.rewind();
+
+                for(int i=0; i<n; i++){
+                    msg = msg + (char) bb.get();
+                }
+
+            }
+            //System.out.println(msg);
+            web = msg;
+            //FileOutputStream fos = new FileOutputStream("info.html");
+            //fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public String getWebsite(){
+        return this.web;
     }
 }
--- src/main/java/com/webscraper/engine/Engine.java
+++ src/main/java/com/webscraper/engine/Engine.java
@@ -1,14 +1,25 @@
 package com.webscraper.engine;
 
 
+import com.webscraper.file.FileManager;
+
 public class Engine {
 
     private String searchEngine;
 
     public Engine(String[] args){
         String link = args[0];
-        DownloadSite downloadSite = new DownloadSite(link);
+        initSiteDownload(link);
+        saveLink(link);
+    }
 
+    private void saveLink(String link){
+        FileManager fileManager = new FileManager();
+        
+    }
+
+    private void initSiteDownload(String link){
+        DownloadSite downloadSite = new DownloadSite(link);
     }
 
 }
